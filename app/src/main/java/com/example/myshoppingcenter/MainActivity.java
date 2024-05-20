c99b55f8612676ed84e1d1dd8e56ab63e18b6067
pyxx2 <873037065@qq.com>
2024-05-20 21:05:30 +0800

    粗略的首页和商品浏览页面

--- app/src/main/java/com/example/myshoppingcenter/MainActivity.java
+++ app/src/main/java/com/example/myshoppingcenter/MainActivity.java
@@ -2,14 +2,16 @@ package com.example.myshoppingcenter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 
---
+++ app/src/main/java/com/example/myshoppingcenter/goods.java
@@ -0,0 +1,4 @@
+package com.example.myshoppingcenter;
+
+public class goods {
+}
